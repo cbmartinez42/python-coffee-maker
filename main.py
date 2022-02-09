641a8de584bedf201c4a77a62718a6d7becd8f96
Chris Martinez <chmartinez@lumeris.com>
2022-02-09 15:29:40 -0600

    breaking out dict and adding functionality to check ingredients

--- main.py
+++ main.py
@@ -1,44 +1,7 @@
-MENU = {
-    "espresso": {
-        "ingredients": {
-            "water": 50,
-            "coffee": 18,
-        },
-        "cost": 1.5,
-    },
-    "latte": {
-        "ingredients": {
-            "water": 200,
-            "milk": 150,
-            "coffee": 24,
-        },
-        "cost": 2.5,
-    },
-    "cappuccino": {
-        "ingredients": {
-            "water": 250,
-            "milk": 100,
-            "coffee": 24,
-        },
-        "cost": 3.0,
-    }
-}
-
-resources = {
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
-}
+from dict import MENU, resources
 
 
-def init():
-    order = input("What would you like? (espresso, latte, cappuccino): ")
-    price = MENU[order]["cost"]
-    print(f"The price for that item is {price}. Please insert your coins:\n")
-    money_handler(price)
-
-
-def money_handler(price):
+def money_handler():
     quarters = int(input("Enter number of quarters: ")) * .25
     dimes = int(input("Enter number of dimes: ")) * .10
     nickels = int(input("Enter number of nickels: ")) * .05
@@ -46,16 +9,32 @@ def money_handler(price):
     deposited = quarters + dimes + nickels + pennies
     if deposited < price:
         print(f"You have deposited {deposited}. That is not enough. ")
-        money_handler(price)
+        money_handler()
     else:
         change = deposited - price
-        make_coffee() if change == 0 else make_change()
+        make_coffee()
+        if change > 0:
+            make_change()
         
 
 def make_coffee():
-
-
+    ingredient_list = []
+    for ingredient in MENU[order]["ingredients"]:
+        if resources.get(ingredient) < MENU[order]["ingredients"].get(ingredient):
+            print(f"We are all out of {ingredient}. Try again tomorrow, sucker. Quitting")
+            exit(42)
+        ingredient_list.append(ingredient)
+    print(ingredient_list)
+    
+    
 def make_change():
+    print("make_change fired")
+    
+    
+# def init():
+order = input("What would you like? (espresso, latte, cappuccino): ")
+price = MENU[order]["cost"]
+print(f"The price for that item is {price}. Please insert your coins:\n")
+money_handler()
 
-
-init()
+# init()
