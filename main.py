4c065448ae68c39a20a3c26868bc3152d2900119
Chris Martinez <chmartinez@lumeris.com>
2022-02-09 16:38:26 -0600

    tinkering

--- main.py
+++ main.py
@@ -1,4 +1,36 @@
-from dict import MENU, resources
+# from dict import MENU, resources
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
 
 
 def money_handler():
@@ -18,13 +50,22 @@ def money_handler():
         
 
 def make_coffee():
-    ingredient_list = []
+    check_ingredients()
+
+    
+def check_ingredients():
     for ingredient in MENU[order]["ingredients"]:
-        if resources.get(ingredient) < MENU[order]["ingredients"].get(ingredient):
+        resource_qty = resources.get(ingredient)
+        order_qty = MENU[order]["ingredients"].get(ingredient)
+        if resource_qty < order_qty:
             print(f"We are all out of {ingredient}. Try again tomorrow, sucker. Quitting")
             exit(42)
+        else:
+            # remainder = resource_qty - order_qty
+            # print("remainder is> ", remainder)
+            resources[ingredient] = resource_qty - order_qty
         ingredient_list.append(ingredient)
-    print(ingredient_list)
+    print(resources)
     
     
 def make_change():
@@ -32,6 +73,7 @@ def make_change():
     
     
 # def init():
+ingredient_list = []
 order = input("What would you like? (espresso, latte, cappuccino): ")
 price = MENU[order]["cost"]
 print(f"The price for that item is {price}. Please insert your coins:\n")
