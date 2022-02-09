7dcd99d574163f742a7a616dd52bd2bcfd74be9b
Chris Martinez <chmartinez@lumeris.com>
2022-02-09 16:39:50 -0600

    tinkering a little more

--- main.py
+++ main.py
@@ -1,36 +1,4 @@
-# from dict import MENU, resources
-MENU = {
-    "espresso": {
-        "ingredients": {
-            "water": 50,
-            "coffee": 18,
-        },
-        "cost": 1.5,
-    },
-    "latte": {
-        "ingredients": {
-            "water": 200,
-            "milk": 150,
-            "coffee": 24,
-        },
-        "cost": 2.5,
-    },
-    "cappuccino": {
-        "ingredients": {
-            "water": 250,
-            "milk": 100,
-            "coffee": 24,
-        },
-        "cost": 3.0,
-    }
-}
-
-resources = {
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
-}
-
+from dict import MENU, resources
 
 
 def money_handler():
