04ce5f31e3ab8a0769dba4c70888a4a928c9b9c6
Chris Martinez <chmartinez@lumeris.com>
2022-02-10 09:20:36 -0600

    adding docstrings

--- main.py
+++ main.py
@@ -1,27 +1,32 @@
 from dict import MENU, resources
+# order = str
+# price = float
 
 
 def money_handler():
-    quarters = int(input("Enter number of quarters: ")) * .25
-    dimes = int(input("Enter number of dimes: ")) * .10
-    nickels = int(input("Enter number of nickels: ")) * .05
-    pennies = int(input("Enter number of pennies: ")) * .01
-    deposited = quarters + dimes + nickels + pennies
-    if deposited < price:
-        print(f"You have deposited {deposited}. That is not enough. ")
+    """Handles taking money """
+    total = int(input("Enter number of quarters: ")) * .25
+    total += int(input("Enter number of dimes: ")) * .10
+    total += int(input("Enter number of nickels: ")) * .05
+    total += int(input("Enter number of pennies: ")) * .01
+
+    if total < float(price):
+        print(f"You have deposited {total}. That is not enough. ")
         money_handler()
     else:
-        change = deposited - price
+        change = total - price
         make_coffee()
         if change > 0:
             make_change()
         
 
 def make_coffee():
+    """Overall function for making coffee"""
     check_ingredients()
 
     
 def check_ingredients():
+    """Standalone function to check for ingredients"""
     for ingredient in MENU[order]["ingredients"]:
         resource_qty = resources.get(ingredient)
         order_qty = MENU[order]["ingredients"].get(ingredient)
@@ -37,12 +42,17 @@ def check_ingredients():
     
     
 def make_change():
+    """Handles making change"""
     print("make_change fired")
     
     
 # def init():
 ingredient_list = []
 order = input("What would you like? (espresso, latte, cappuccino): ")
+if order == "resources":
+    for item in resources:
+        print(resources)
+        # init()
 price = MENU[order]["cost"]
 print(f"The price for that item is {price}. Please insert your coins:\n")
 money_handler()
